46fa7a7631ed8be8ba457adf35a038637d512138
entity105 <nikchakal@inbox.ru>
2025-11-30 18:53:00 +0300

    Чистка кода
    Убран лишний закомментированный код. Добавлены комментарии к методам

--- gameplay.py
+++ gameplay.py
@@ -14,10 +14,12 @@ class BattleShip:
 
     @staticmethod
     def auto(pole: GamePole):
+        """Авто расстановка кораблей для заданного поля"""
         pole.init()
 
     @staticmethod
     def set_ship(pole: GamePole):
+        """Самостоятельная расстановка"""
         pole.set_ship()
 
     @property
@@ -38,19 +40,7 @@ class BattleShip:
                 for i, coord in enumerate(coords):
                     if x_y == coord:
                         return ship, i
-        # raise Exception("Не найден корабль (метод find_ship)")
-        # else:   # отладка
-        #     print(f'Матричные координаты корабля, в которого попали: {x_y0}\n')
-        #     print(f'Преобразованные координаты: {x_y}\n')
-        #     print('Поле:')
-        #     print('***********************************************')
-        #     pole_obj.show()
-        #     print('***********************************************')
-        #     print('Координаты всех кораблей:')
-        #     for ship_d in pole_obj.get_ships():
-        #         print(tuple(ship_d.get_cords()))
-        #     raise ValueError
-
+        raise Exception("Не найден корабль (метод find_ship)")
 
     def smart_shooting(self, x0, y0, pole_obj):
         """True - уничтожил корабль, False - промах"""
@@ -72,7 +62,6 @@ class BattleShip:
 
 
         if damage != 2:         # Промах (ошибся ориентацией)
-            # self.start_cords_p = x0, y0  # Сохраняем исходные координаты
             self.start_cords_s = x0, y0
             return False
 
@@ -92,7 +81,6 @@ class BattleShip:
                 break
 
             damage = self.shot(x, y, pole_obj)  # Стреляем
-
 
         if not damage:      # если попытался стрельнуть в закрытую клетку
             x = x0 - dx     # Отступаем от начала в противоположную сторону
@@ -116,10 +104,8 @@ class BattleShip:
         self.start_cords_p = None
         return False
 
-
-
     def autoshot(self, pole_obj):
-        """Серия выстрелов компьтера"""
+        """Серия умных выстрелов компьтера"""
         while True:
             if self.start_cords_s is None and self.start_cords_p is None:     # если это не продолжение серии выстрелов
                 x = randint(1, pole_obj.size)       # берём произвольные x y
@@ -148,28 +134,20 @@ class BattleShip:
         place = pole_obj.pole[y][x]
         if place == 0:
             pole_obj.pole[y][x] = 3
-            # print()
-            # pole_obj.show()
             return 1
         elif place == 1:
-            # print(x, y, sep=' ')
             pole_obj.pole[y][x] = 2
-            # print(self.find_ship((x, y), pole_obj))
             ship, i = self.find_ship((x, y), pole_obj)  # Вводим матричные координаты
             ship[i] = 2
             if ship.is_destroyed():
                 for x, y in ship.place_around():
                     pole_obj.pole[-y][x-1] = 3
-            # print()
-            # pole_obj.show()
             return 2
         else: # если там 2 или 3
-            # print('Координаты, где False')
-            # print(x, y, sep=' ')
             return False
 
     def game(self, p_1, p_2):
-        while not all(map(lambda x: x.cells.count(2) == x.get_length(), p_1.get_ships())) and not all(map(lambda x: x.cells.count(2) == x.get_length(), p_2.get_ships())):
+        while not all(map(lambda x: x.is_destroyed(), p_1.get_ships())) and not all(map(lambda x: x.is_destroyed(), p_2.get_ships())):
             # count_strike1 = self.shot(int(input()), int(input()), p_1)
             # while count_strike1 == 2:
             #     count_strike1 = self.shot(int(input()), int(input()), p_1)
@@ -181,7 +159,7 @@ class BattleShip:
             count_strike2 = self.autoshot(p_2)
             while count_strike2 == 2:
                 count_strike2 = self.autoshot(p_2)
-
+        pass
 
 
 SIZE = 10
